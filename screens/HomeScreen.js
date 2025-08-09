9f6dcbf6fc15e1b6792730d1f09be3773597b908
pratham3w <pratham@triplewsols.com>
2025-08-09 17:37:51 +0530

    setup react navigation

---
+++ screens/HomeScreen.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {View,Text} from "react-native"
+import { SafeAreaView } from 'react-native-safe-area-context'
+const HomeScreen = () => {
+  return (
+//     <SafeAreaView>
+
+// <Text>
+//     Home Scrren
+
+//     </Text>
+//     </SafeAreaView>
+<View>
+
+<Text>
+    Home Scrren
+
+    </Text>
+    </View>
+  )
+}
+
+export default HomeScreen
